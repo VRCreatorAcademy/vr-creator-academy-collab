7856aec6fcc38207174e242a1eba32a5f9ab4db4
Jacob Christ <jacob@pontech.com>
2022-09-25 19:55:06 -0700

    Cornhole after adding Golden Path Module Two (with only some slight naming differences)

---
+++ vr-creator-academby-collab-unity-project/Assets/Core/Scripts/Networking/NetworkVariableTest.cs
@@ -0,0 +1,33 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkVariableTest : NetworkBehaviour
+{
+    private NetworkVariable<float> ServerNetworkVariable = new NetworkVariable<float>();
+    private NetworkVariable<float> ClientNetworkVariable = new NetworkVariable<float>();
+    private float last_t = 0.0f;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            ServerNetworkVariable.Value = 0.0f;
+            Debug.Log("Server's var initialized to: " + ServerNetworkVariable.Value);
+        }
+    }
+
+    void Update()
+    {
+        var t_now = Time.time;
+        if (IsServer)
+        {
+            ServerNetworkVariable.Value = ServerNetworkVariable.Value + 0.1f;
+            if (t_now - last_t > 0.5f)
+            {
+                last_t = t_now;
+                // Debug.Log("Server set its var to: " + ServerNetworkVariable.Value + ", has client var at: " + ClientNetworkVariable.Value);
+                Debug.Log("Server set its var to: " + ServerNetworkVariable.Value + ", has client var at: " + "ClientNetworkVariable not defined");
+            }
+        }
+    }
+}

d4e3b628ed77c2deb3202f7d0aeef80869f8bc98
David <djhatvr@gmail.com>
2022-10-10 17:11:09 -0700

    Working Avatar: added CornHole Scene Clone to test Avatar, added Closed Circuit TV to view Avatar, added asmdef, Upgraded to XR Interation Toolkey 2.2.1, Moved Animation into Animation folder, Added Dave's Tools,

--- vr-creator-academby-collab-unity-project/Assets/_David Avatar/Scripts/PlayerController.cs
+++ vr-creator-academby-collab-unity-project/Assets/_David Avatar/Scripts/PlayerController.cs
@@ -2,34 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerController : MonoBehaviour
 {
-	private Animator				animator;
-	// Start is called before the first frame update
+	[SerializeField] XRController		leftController;
+	[SerializeField] XRController		rightController;
+	private Animator					animator;
+
+	List<UnityEngine.XR.InputDevice> leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+	List<UnityEngine.XR.InputDevice> rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+
+
 	void Start()
 	{
-	animator = GetComponent<Animator>();
+		animator = GetComponentInChildren<Animator>();
+
 	}
 
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		GetDevices();
+		LeftGrip();
+		RightGrip();
+	}
+
+	void GetDevices()
+	{
+		if(rightHandDevices.Count == 0)
+		{
+			var leftCharacteristics = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+			InputDevices.GetDevicesWithCharacteristics(leftCharacteristics, leftHandDevices);
+
+			if(leftHandDevices.Count == 1)
+			{
+				UnityEngine.XR.InputDevice device = leftHandDevices[0];
+				Debug.Log(string.Format("Left Hand Device name '{0}' with role '{1}'", device.name, device.characteristics.ToString()));
+			}
+		}
+
+		if(rightHandDevices.Count == 0)
+		{
+			var rightCharacteristics = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Right| InputDeviceCharacteristics.Controller;
+			InputDevices.GetDevicesWithCharacteristics(rightCharacteristics, rightHandDevices);
+
+			if(rightHandDevices.Count == 1)
+			{
+				UnityEngine.XR.InputDevice device = rightHandDevices[0];
+				Debug.Log(string.Format("Right Hand Device name '{0}' with role '{1}'", device.name, device.characteristics.ToString()));
+			}
+		}
 	}
 
 
-	public void OnLeftGrip(InputAction.CallbackContext context)
+public void LeftGrip()
 	{
-		float grip = context.ReadValue<float>();
-		animator.SetFloat("Left Grip", grip);
+		if(leftHandDevices.Count!=0)
+		{
+			float gripValue;
+			leftHandDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out gripValue);
+			Debug.Log($"Grip = {gripValue}");
+			animator.SetFloat("Left Grip", gripValue);
+		}
 	}
 
 
-	public void OnRightGrip(InputAction.CallbackContext context)
+	public void RightGrip()
 	{
-		float grip = context.ReadValue<float>();
-		animator.SetFloat("Right Grip", grip);
+		if(rightHandDevices.Count != 0)
+		{
+			float gripValue;
+			rightHandDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out gripValue);
+			Debug.Log($"Grip = {gripValue}");
+			animator.SetFloat("Right Grip", gripValue);
+		}
 	}
 }

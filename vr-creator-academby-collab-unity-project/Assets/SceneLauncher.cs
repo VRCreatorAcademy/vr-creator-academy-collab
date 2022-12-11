00080f16726f3227cab7c3967527b3f5b8da57d9
Jacob Christ <jacob@pontech.com>
2022-12-11 11:52:06 -0800

    Added comments / todo for protecting loading a scene if the network managers is not running.

--- vr-creator-academby-collab-unity-project/Assets/SceneLauncher.cs
+++ vr-creator-academby-collab-unity-project/Assets/SceneLauncher.cs
@@ -1,10 +1,14 @@
-using UnityEngine;
+using Unity.Netcode;
 using UnityEngine.SceneManagement;
 
-public class SceneLauncher : MonoBehaviour
+public class SceneLauncher : NetworkBehaviour
 {
     public void ChangeScene(string SceneName)
     {
-        SceneManager.LoadScene(SceneName);
+      // todo 2: Do not try to access the NetworkSceneManager when the NetworkManager
+      // is shutdown.The NetworkSceneManager is only instantiated when a NetworkManager
+      // is started.This same rule is true for all Netcode systems that reside within
+      // the NetworkManager.
+      NetworkManager.Singleton.SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
     }
 }
